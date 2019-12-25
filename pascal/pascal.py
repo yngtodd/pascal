36b1cd06ca6387ebff51c3247f86138123635954
Young, Todd <youngmt1@ornl.gov>
2019-12-24 19:14:50 -0500

    Create a basic calculator
    Super limited, but working.

--- pascal/pascal.py
+++ pascal/pascal.py
@@ -1,3 +1,22 @@
 # -*- coding: utf-8 -*-
+from pascal.core.interpreter import Interpreter
 
-"""Main module."""
+
+def main():
+    while True:
+        try:
+            text = input('calc> ')
+        except EOFError:
+            break
+
+        if not text:
+            continue
+
+        interpreter = Interpreter(text)
+        result = interpreter.expr()
+        print(result)
+
+
+if __name__=='__main__':
+    main()
+
