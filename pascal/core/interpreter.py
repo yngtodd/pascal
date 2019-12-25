ef054bfcf0f906bd4e9193342a25a0a922dbb0b6
Young, Todd <youngmt1@ornl.gov>
2019-12-24 23:21:06 -0500

    Add support for multiple terms
    We can now do longer expressions with basic arithmetic. Still
    need the order of operations.

--- pascal/core/interpreter.py
+++ pascal/core/interpreter.py
@@ -10,6 +10,7 @@ class Interpreter:
 
     def _reset(self):
         self.pos = 0
+        self.current_char = self.text[self.pos] 
         self.current_token = None
 
     def get_next_token(self):
@@ -17,23 +18,53 @@ class Interpreter:
        
         Splits up a sentence into tokens.
         """
-        if self.pos > len(self.text)-1:
-            return Token(Type.EOF, None)
+        while self.current_char is not None:
 
-        current_char = self.text[self.pos]
+            if self.current_char.isspace():
+                self.skip_whitespace()
+                continue
 
-        if current_char.isdigit():
-            token = Token(Type.INTEGER, int(current_char))
-            self.pos += 1
-            return token
+            if self.current_char.isdigit():
+                return Token(Type.INTEGER, self.integer())
 
-        if current_char == '+':
-            token = Token(Type.PLUS, current_char)
-            self.pos += 1
-            return token
+            if self.current_char == '+':
+                self.advance()
+                return Token(Type.PLUS, self.current_char)
+
+            if self.current_char == '-':
+                self.advance()
+                return Token(Type.MINUS, self.current_char)
+
+            if self.current_char == '*':
+                self.advance()
+                return Token(Type.MULTIPLY, self.current_char)
+ 
+            if self.current_char == '/':
+                self.advance()
+                return Token(Type.DIVIDE, self.current_char)
         
-        raise ValueError(f"Unknown token type: {current_char}")
+            raise ValueError(f"Unknown token type: {current_char}")
 
+        return Token(Type.EOF, None)
+
+    def integer(self):
+        result = ''
+        while self.current_char is not None and self.current_char.isdigit():
+            result += self.current_char
+            self.advance()
+        return int(result)
+
+    def skip_whitespace(self):
+        while self.current_char is not None and self.current_char.isspace():
+            self.advance()
+
+    def advance(self):
+        """ Move to the next character """
+        self.pos += 1
+        if self.pos > len(self.text)-1:
+            self.current_char = None
+        else:
+            self.current_char = self.text[self.pos]
 
     def eat(self, token_type):
         """ """
@@ -42,16 +73,31 @@ class Interpreter:
         else:
             raise ValueError(f"Improper token type!")
 
+    def term(self):
+        token = self.current_token
+        self.eat(Type.INTEGER)
+        return token.value
+
     def expr(self):
         self.current_token = self.get_next_token()
 
-        left = self.current_token
-        self.eat(Type.INTEGER)
+        result = self.term()
+        while self.current_token.type in (
+                Type.PLUS, Type.MINUS, Type.MULTIPLY, Type.DIVIDE):
 
-        op = self.current_token
-        self.eat(Type.PLUS)
+            token = self.current_token
 
-        right = self.current_token
-        self.eat(Type.INTEGER)
+            if token.type == Type.PLUS:
+                self.eat(Type.PLUS)
+                result += self.term()
+            elif token.type == Type.MINUS:
+                self.eat(Type.MINUS)
+                result -= self.term()
+            elif token.type == Type.MULTIPLY:
+                self.eat(Type.MULTIPLY)
+                result *= self.term()
+            elif token.type == Type.DIVIDE:
+                self.eat(Type.DIVIDE)
+                result /= self.term()
 
-        return left.value + right.value
+        return result
--- pascal/core/types.py
+++ pascal/core/types.py
@@ -4,4 +4,7 @@ from enum import Enum
 class Type(Enum):
     INTEGER = "INTEGER"
     PLUS = "PLUS"
+    MINUS = "MINUS"
+    MULTIPLY = "MULTIPLY"
+    DIVIDE = "DIVIDE"
     EOF = "EOF"
